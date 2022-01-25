671b75eb2656fc85545d73c7794e2ea265bdae81
amirmoumen <amirmoumen06@yahoo.fr>
2022-01-25 10:19:46 -0800

    tailwind config files added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'cusWhite': '#e9e8e8',
+      },
+    },
+  },
+  plugins: [],
+}
